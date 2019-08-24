9ecb33f74c70423e360fda7c149212f5870d3c86
kearron <kearon99@gmail.com>
2019-08-24 09:09:30 +0545

    count each word in file

---
+++ task2/word_count.py
@@ -0,0 +1,21 @@
+# SubTask 2:
+# 1.Find word count,
+
+# find frequency of top 100 words excluding stop words,
+# find the longest and shortest word and there frequency,
+# find top 100 verbs, nouns, adjectives and adverbs,find their composition of text based on part of speech ( like 10% is noun, 20% is verb ..) ,
+# find top 100 noun-noun phrase and noun-verb pair .
+import re
+
+word_count = {}
+with open('./war_n_peace.txt','r') as file:
+   for line in file:
+       words = line.split(" ")
+       for word in words:
+           word = re.sub('[^A-Za-z]','',word) #remove character other than A-Z, a-z
+           if word not in word_count:
+               word_count[word] = 1
+           else:
+               word_count[word] += 1
+   for key,value in word_count.items():
+       print(key,": ",value)
